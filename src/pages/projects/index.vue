ee4a3dac9f303dad4eb3279e10c42df565e307b1
Ruben <r.bojorquez@cyberpuerta.mx>
2025-01-13 23:49:22 -0700

    chore:  -

---
+++ src/pages/projects/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
+import type { Tables } from '../../../database/types'
+
+const projects = ref<Tables<'projects'>[] | null>(null)
+
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.error(error)
+
+  projects.value = data
+
+  console.log('projects', projects.value)
+})()
+</script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
+  </div>
+</template>
