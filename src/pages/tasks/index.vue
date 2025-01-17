f82d433bcc05449e4c9167e25a5d9b07d6e1d78c
Ruben <r.bojorquez@cyberpuerta.mx>
2025-01-16 17:26:37 -0700

    chore: Retrieve Project Details for Tasks: Querying Nested Database Tables -

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -9,7 +9,14 @@ usePageStore().pageData.title = 'Tasks'
 const tasks = ref<Tables<'tasks'>[] | null>(null)
 
 const getTasks = async () => {
-  const { data, error } = await supabase.from('tasks').select()
+  const { data, error } = await supabase.from('tasks').select(`
+    *,
+    projects (
+      id,
+      name,
+      slug
+    )
+  `)
 
   if (error) console.error(error)
 
@@ -47,10 +54,14 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     }
   },
   {
-    accessorKey: 'project_id',
+    accessorKey: 'projects',
     header: () => h('div', { class: 'text-left' }, 'Project'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
+      return h(
+        RouterLink,
+        { to: `/projects/${row.original.projects.slug}`, class: 'text-left font-medium' },
+        () => row.getValue('projects').name
+      )
     }
   },
   {
