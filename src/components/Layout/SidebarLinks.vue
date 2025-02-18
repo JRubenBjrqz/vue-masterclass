f3bc0aa421d74756fa932381648570ff92100ab0
Ruben <r.bojorquez@cyberpuerta.mx>
2025-02-17 20:39:23 -0700

    chore: Use the Vue.js Template Special Element with v-for Loops -

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 interface LinkProp {
   title: string
   to?: string
@@ -9,30 +8,24 @@ interface LinkProp {
 const props = defineProps<{
   links: LinkProp[]
 }>()
-
-const filteredLink = props.links.filter((link): link is LinkProp & { to: string } => !!link.to)
 </script>
 
 <template>
-  <RouterLink
-    exactActiveClass="text-primary bg-muted"
-    v-for="link in filteredLink"
-    :key="link.title"
-    :to="link.to"
-    class="nav-link"
-  >
-    <iconify-icon :icon="link.icon"></iconify-icon>
-    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
-  </RouterLink>
+  <template v-for="link in links" :key="link.title">
+    <RouterLink v-if="link.to" exactActiveClass="text-primary bg-muted" :to="link.to" class="nav-link">
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden text-nowrap lg:block">{{ link.title }}</span>
+    </RouterLink>
 
-  <div class="nav-link cursor-pointer">
-    <!-- <iconify-icon :icon="link.icon"></iconify-icon>
-    <span class="hidden lg:block text-nowrap">{{ link.title }}</span> -->
-  </div>
+    <div v-else class="nav-link cursor-pointer">
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </div>
+  </template>
 </template>
 
 <style scoped>
 .nav-link {
-  @apply flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground
+  @apply mx-2 flex items-center justify-center gap-3 rounded-lg px-4 py-2 text-muted-foreground transition-colors hover:text-primary lg:justify-normal;
 }
 </style>
