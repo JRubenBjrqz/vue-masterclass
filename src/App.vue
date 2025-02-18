5fa0fad693652bb754d58466aa647ab4ac8252fb
Ruben <r.bojorquez@cyberpuerta.mx>
2025-02-18 01:53:19 -0700

    chore: Watch for Supabase Auth changes and Update Auth Store -

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
 const errorStore = useErrorStore()
 
 onErrorCaptured((error) => {
   errorStore.setError({ error })
 })
 
-onMounted(async () => {
+onMounted(() => {
+  useAuthStore().trackAuthChanges()
 })
 </script>
 
 <template>
   <AuthLayout>
-
     <AppErrorPage v-if="errorStore.activeError" />
 
     <RouterView v-else v-slot="{ Component, route }">
       <Suspense v-if="Component" timeout="0">
-        <Component
-          :is="Component"
-          :key="route.name"
-        />
+        <Component :is="Component" :key="route.name" />
 
         <template #fallback>
           <div>
