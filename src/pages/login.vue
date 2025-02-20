b6b3599cf8e42aefd5b616eea5fe2f4621919d7c
Ruben <r.bojorquez@cyberpuerta.mx>
2025-02-19 23:30:26 -0700

    chore: Handle Supabase Server Auth Errors -

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -6,11 +6,15 @@ const formData = ref({
   password: '',
 })
 
+const _error = ref('')
+
 const router = useRouter()
 
 const signin = async () => {
-  const isLoggedIn = await login(formData.value)
-  if (isLoggedIn) router.push('/')
+  const {error} = await login(formData.value)
+  if (!error) return router.push('/')
+
+  _error.value = error.message == 'Invalid login credentials' ? 'Invalid email or password' : error.message
 }
 </script>
 
@@ -30,15 +34,31 @@ const signin = async () => {
         <form class="grid gap-4" @submit.prevent="signin">
           <div class="grid gap-2">
             <Label id="email" class="text-left">Email</Label>
-            <Input type="email" placeholder="johndoe19@example.com" required v-model="formData.email" />
+            <Input
+              type="email"
+              placeholder="johndoe19@example.com"
+              required
+              v-model="formData.email"
+              :class="{'border-red-500': _error}"
+            />
           </div>
           <div class="grid gap-2">
             <div class="flex items-center">
               <Label id="password">Password</Label>
               <a href="#" class="inline-block ml-auto text-xs underline"> Forgot your password? </a>
             </div>
-            <Input id="password" type="password" autocomplete required v-model="formData.password" />
+            <Input
+              id="password"
+              type="password"
+              autocomplete="off"
+              required
+              v-model="formData.password"
+              :class="{'border-red-500': _error}"
+            />
           </div>
+          <ul class="text-sm text-left text-red-500" v-if="_error">
+            <li class="list-disc">{{ _error }}</li>
+          </ul>
           <Button type="submit" class="w-full"> Login </Button>
         </form>
         <div class="mt-4 text-sm text-center">
