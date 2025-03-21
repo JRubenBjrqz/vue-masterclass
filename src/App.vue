6daa2b25e467e43bab2614b1c1e5475d6d72c9fd
Ruben <r.bojorquez@cyberpuerta.mx>
2025-03-20 22:31:49 -0700

    chore: Render the Appropriate Layout -

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-
 const errorStore = useErrorStore()
 
 onErrorCaptured((error) => {
@@ -10,10 +8,15 @@ onErrorCaptured((error) => {
 onMounted(() => {
   useAuthStore().trackAuthChanges()
 })
+
+const { user } = storeToRefs(useAuthStore())
+
+const AuthLayout = defineAsyncComponent(() => import('@/components/Layout/main/AuthLayout.vue'))
+const GuestLayout = defineAsyncComponent(() => import('@/components/Layout/main/GuestLayout.vue'))
 </script>
 
 <template>
-  <AuthLayout>
+  <Component :is="user ? AuthLayout : GuestLayout">
     <AppErrorPage v-if="errorStore.activeError" />
 
     <RouterView v-else v-slot="{ Component, route }">
@@ -27,5 +30,5 @@ onMounted(() => {
         </template>
       </Suspense>
     </RouterView>
-  </AuthLayout>
+  </Component>
 </template>
